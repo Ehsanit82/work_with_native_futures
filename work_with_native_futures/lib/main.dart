6c452cb0c13f596a48b7f38fa36577f908b45261
Ehsan <ehsanabbasi840@yahoo.com>
2023-05-22 15:40:41 +0430

    init project

--- work_with_native_futures/lib/main.dart
+++ work_with_native_futures/lib/main.dart
@@ -8,19 +8,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Great Plces',
       theme: ThemeData(
-          primaryColor: const Color(0xff04075f), backgroundColor: Colors.white),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Great Plces',
-            style: TextStyle(color: Theme.of(context).backgroundColor),
-          ),
-          backgroundColor: Theme.of(context).primaryColor,
-        ),
-        body: Text(
-          'hello',style: TextStyle(color: Theme.of(context).primaryColor),
-        ),
-      ),
+          primaryColor: const Color.fromARGB(255, 23, 0, 80),
+          canvasColor: Colors.white),
     );
   }
 }
