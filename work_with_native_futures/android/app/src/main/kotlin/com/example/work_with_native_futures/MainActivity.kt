4a0d9d319ce584358796d32b1ee2169b4c41d0ed
Ehsan <ehsanabbasi840@yahoo.com>
2023-05-22 15:13:43 +0430

    first commit

---
+++ work_with_native_futures/android/app/src/main/kotlin/com/example/work_with_native_futures/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.work_with_native_futures
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ work_with_native_futures/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Great Plces',
+      theme: ThemeData(
+          primaryColor: const Color(0xff04075f), backgroundColor: Colors.white),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Great Plces',
+            style: TextStyle(color: Theme.of(context).backgroundColor),
+          ),
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+        body: Text(
+          'hello',
+        ),
+      ),
+    );
+  }
+}
