2edbe064f9669a771316bbe8c9123e617ea0e071
Ehsan <ehsanabbasi840@yahoo.com>
2023-05-22 17:17:56 +0430

    init project

--- work_with_native_futures/lib/providers/great_places.dart
+++ work_with_native_futures/lib/providers/great_places.dart
@@ -4,7 +4,7 @@ import '../models/place.dart';
 class GreatPlaces with ChangeNotifier {
   final List<Place> _items=[];
 
-  List<Place> get items{
+  List<Place> get items {
     return[..._items];
   }
 }
